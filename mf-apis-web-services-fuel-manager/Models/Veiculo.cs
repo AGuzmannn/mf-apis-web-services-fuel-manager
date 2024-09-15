97ecde1da4d70a8ff1dff17fa719f3266a84a5f6
Alexandre Guzmán <aguzmannn@gmail.com>
2024-09-15 18:14:13 -0300

    Criação e configuração dos modelos de Veículo e Consumo

---
+++ mf-apis-web-services-fuel-manager/Models/Veiculo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]  
+        public string Marca { get; set; }
+
+
+        [Required]
+        public string Modelo { get; set; }
+
+        [Required]
+        public string Placa { get; set; }
+
+        [Required]
+        public string AnoFabricacao { get; set; }
+
+        [Required]
+        public string AnoModelo { get; set; }
+
+        public ICollection<Consumo> Consumos { get; set; }
+    }
+}
